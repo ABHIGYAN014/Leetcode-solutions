1646684eaa86f88c21b116c00e5143094198d808
Abhigyan Singh <124283757+ABHIGYAN014@users.noreply.github.com>
2024-10-26 23:15:51 +0530

    Time: 0 ms (100%), Space: 16.7 MB (94.97%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        // Flags to check if first row or first column needs to be zero
+        bool firstRowZero = false;
+        bool firstColZero = false;
+
+        // Check if the first row has any zero
+        for (int j = 0; j < n; ++j) {
+            if (matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        // Check if the first column has any zero
+        for (int i = 0; i < m; ++i) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+
+        // Use first row and column as markers for the rest of the matrix
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Set rows based on markers in the first column
+        for (int i = 1; i < m; ++i) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < n; ++j) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Set columns based on markers in the first row
+        for (int j = 1; j < n; ++j) {
+            if (matrix[0][j] == 0) {
+                for (int i = 1; i < m; ++i) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Set the first row to zero if needed
+        if (firstRowZero) {
+            for (int j = 0; j < n; ++j) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Set the first column to zero if needed
+        if (firstColZero) {
+            for (int i = 0; i < m; ++i) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
