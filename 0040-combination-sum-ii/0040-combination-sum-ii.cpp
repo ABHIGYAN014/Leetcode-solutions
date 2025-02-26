94456e44c09e6741596abaf21f2aaec1103bb349
Abhigyan Singh <124283757+ABHIGYAN014@users.noreply.github.com>
2025-02-26 19:40:44 +0530

    Time: 0 ms (100%), Space: 14.1 MB (38.43%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    void solve(vector<int>& candidates, int target,vector<vector<int>>&ans,vector<int>&v,int index)
+    {
+        if(target<0)
+        {
+            return;
+        }
+        if(target==0)
+        {
+            ans.push_back(v);
+            return;
+        }
+
+        for(int i=index;i<candidates.size();i++)
+        {
+            if(i>index && candidates[i]==candidates[i-1])
+            {
+                continue;
+            }
+            v.push_back(candidates[i]);
+            solve(candidates,target-candidates[i],ans,v,i+1);
+            v.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        //Sort krr dete hai for clearly identfying th edublicates
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>>ans;
+        vector<int>v;
+        solve(candidates,target,ans,v,0);
+
+        // // TO remove al the dublicates just pushing all these in an set
+        // set<vector<int>>st;
+        // for(auto e:ans)
+        // {
+        //     st.insert(e);
+        // }
+        // ans.clear();
+        // for(auto e:st)
+        // {
+        //     ans.push_back(e);
+        // }
+        return ans;
+    }
+};
