0b4af8a9996d7f5300f5e8efdb4a349dd39e097d
Abhigyan Singh <124283757+ABHIGYAN014@users.noreply.github.com>
2025-02-27 23:19:48 +0530

    Time: 0 ms (100%), Space: 9.8 MB (33.54%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> solve(int row) {
+        long long ans = 1;
+        vector<int> rowans;
+        rowans.push_back(1);
+        
+        for(int col = 1; col < row; col++) {
+            ans = ans * (row - col);
+            ans = ans / col;
+            rowans.push_back(ans);
+        }
+        
+        return rowans;
+    }
+    
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        for(int i = 0; i < numRows; i++) { 
+            ans.push_back(solve(i + 1));   
+        }
+        return ans;
+    }
+};
