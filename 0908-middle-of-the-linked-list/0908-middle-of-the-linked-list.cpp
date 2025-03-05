7523afb09a986f23d8f3b788ea3aaa17743192e1
Abhigyan Singh <124283757+ABHIGYAN014@users.noreply.github.com>
2025-03-05 23:40:40 +0530

    Time: 0 ms (100%), Space: 10 MB (25.61%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
@@ -11,32 +11,42 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        int length=0;
-        ListNode*temp=head;
-        ListNode*temp1=head;
-        // ListNode*temp2=tail;
+        
+        ListNode*slow=head;
+        ListNode*fast=head;
+        while(fast!=NULL &&fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
 
+
+        //recursive
+        // int length=0;
+        // ListNode*temp=head;
         // while(temp1==temp2)
         // {
         //     temp1=temp1->next;}
-        while(temp!=NULL)
-        {
-            temp=temp->next;
-            length++;
-        }
-        int middle;
-        if(length%2==0)
-        {
-             middle=(length/2)+1;
-        }
-        else
-        {
-            middle=(length+1)/2;
-        }
-        for(int i=0;i<middle-1;i++)
-        {
-            temp1=temp1->next;
-        }
-        return temp1;
+
+    //     while(temp!=NULL)
+    //     {
+    //         temp=temp->next;
+    //         length++;
+    //     }
+    //     int middle;
+    //     if(length%2==0)
+    //     {
+    //          middle=(length/2)+1;
+    //     }
+    //     else
+    //     {
+    //         middle=(length+1)/2;
+    //     }
+    //     for(int i=0;i<middle-1;i++)
+    //     {
+    //         temp1=temp1->next;
+    //     }
+    //     return temp1;
     }
 };
