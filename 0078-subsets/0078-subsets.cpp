4da960a89269b83f22eee4e9c27c8fc3af277507
Abhigyan Singh <124283757+ABHIGYAN014@users.noreply.github.com>
2025-06-12 15:18:16 +0530

    Time: 0 ms (100%), Space: 9.9 MB (81.03%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     void backtrack(vector<int>& nums, int index, vector<int>& current, vector<vector<int>>& result) {
-        // Add the current subset to the result
+
         result.push_back(current);
 
-        // Explore further by including each element one by one
         for (int i = index; i < nums.size(); i++) {
-            // Include the current element
             current.push_back(nums[i]);
-
-            // Recur to generate further subsets
             backtrack(nums, i + 1, current, result);
-
-            // Backtrack and remove the element to explore other possibilities
             current.pop_back();
         }
     }
