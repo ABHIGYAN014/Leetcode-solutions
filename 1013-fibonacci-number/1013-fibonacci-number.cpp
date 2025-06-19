86d38c58ea5852b75e0666ca3729e2dc6ee62cde
Abhigyan Singh <124283757+ABHIGYAN014@users.noreply.github.com>
2025-06-19 23:02:01 +0530

    Time: 0 ms (100%), Space: 8.3 MB (11.4%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
+    int solve(int n,vector<int>&dp)
+    {
+        //base case
+        if(n==0 || n==1){
+        return n;
+        }
+
+        //using memoization that if alreeady found dont search for it 
+        if(dp[n]!=-1)
+        {
+            return dp[n];
+        }
+        dp[n]=solve(n-1,dp)+solve(n-2,dp);
+        return dp[n];
+    }
     int fib(int n) {
-        if(n==0)
-        {
-        return 0;
-        }
-        if(n==1)
-        {
-            return 1;
-        }
-           
-        int ans= fib(n-1)+fib(n-2);
-        return ans ;
+       vector<int>dp(n+1,-1);
+        int ans=solve(n,dp);
+
+        return ans;
     }
 };
