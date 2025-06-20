c201003d656f63595477fd11790e4cbfb10b0e3f
Abhigyan Singh <124283757+ABHIGYAN014@users.noreply.github.com>
2025-06-20 22:24:05 +0530

    Time: 0 ms (100%), Space: 10.6 MB (45.78%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-         if (nums.empty()) return 0;
-        if (nums.size() == 1) return nums[0];
-        
-        // Initialize two variables to store the maximum money we can rob
-        int prev1 = 0; // Maximum money robbed till the previous house
-        int prev2 = 0; // Maximum money robbed till the house before the previous house
-        
-        for (int num : nums) {
-            int current = std::max(prev1, prev2 + num); // Current maximum money that can be robbed
-            prev2 = prev1; // Move prev1 to prev2
-            prev1 = current; // Update prev1 to current
+    int solve(int n,vector<int>&nums,vector<int>&dp,int i)
+    {
+        if(i>=n)
+        {
+            return 0;
         }
-        
-        return prev1; // 
+
+        if(dp[i]!=INT_MIN)
+        {
+            return dp[i];
+        }
+
+        int opt1=nums[i]+solve(n,nums,dp,i+2);
+        int opt2=0+solve(n,nums,dp,i+1);
+
+        dp[i]=max(opt1,opt2);
+
+        return dp[i];
+    }
+        int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n+1,INT_MIN);
+        int i=0;
+        return solve(n,nums,dp,i);
     }
 };
