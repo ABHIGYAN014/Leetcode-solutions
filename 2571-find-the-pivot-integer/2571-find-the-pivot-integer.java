71ada762449f31f06f19e9812c0d4a2a2e95b7f4
Abhigyan Singh <124283757+ABHIGYAN014@users.noreply.github.com>
2025-06-21 10:29:05 +0530

    Time: 1 ms (71.09%), Space: 40.5 MB (80.54%) - LeetHub

---
+++ 2571-find-the-pivot-integer/2571-find-the-pivot-integer.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int i=1;
+        int j=n;
+
+        int left_sum=i;
+        int right_sum=n;
+
+        while(i<j)
+        {
+            if(left_sum<right_sum)
+            {
+                i++;
+                left_sum+=i;
+            }
+
+            else
+            {
+                j--;
+                right_sum+=j;
+            }
+        }
+
+        if(left_sum==right_sum)
+        {
+            return i;
+        }
+        return -1;
+    }
+}
