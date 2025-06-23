6e71a31b9172ff16ed46a590ed3e3eb4708eb3a2
Abhigyan Singh <124283757+ABHIGYAN014@users.noreply.github.com>
2025-06-23 15:34:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -3,12 +3,12 @@ public:
   int solve(int i, vector<int>& cost, vector<int>& dp) {
     int n = cost.size();
     if (i >= n) return 0;
-    if (dp[i] != -1) return dp[i];
+    // if (dp[i] != -1) return dp[i];
 
-    int oneStep = cost[i] + solve(i + 1, cost, dp);
-    int twoStep = cost[i] + solve(i + 2, cost, dp);
-
-    return dp[i] = min(oneStep, twoStep);
+    int one = cost[i] + solve(i + 1, cost, dp);
+    int two = cost[i] + solve(i + 2, cost, dp);
+    int ans=min(one,two);
+    return ans;
 }
 
 int minCostClimbingStairs(vector<int>& cost) {
