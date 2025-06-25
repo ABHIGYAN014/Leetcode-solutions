de77468bd7d1cc42afa65e3f4d2cbb51dba82573
Abhigyan Singh <124283757+ABHIGYAN014@users.noreply.github.com>
2025-06-25 12:10:23 +0530

    Time: 0 ms (100%), Space: 14 MB (87.59%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,25 +1,45 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int prev, int curr, vector<vector<int>>& dp) {
-        if (curr >= nums.size())
-            return 0;
+    // int solve(vector<int>& nums, int prev, int curr, vector<vector<int>>& dp) {
+    //     if (curr >= nums.size())
+    //         return 0;
 
-        if (dp[curr][prev + 1] != -1)
-            return dp[curr][prev + 1];
+    //     if (dp[curr][prev + 1] != -1)
+    //         return dp[curr][prev + 1];
 
-        int include = 0;
-        if (prev == -1 || nums[prev] < nums[curr]) {
-            include = 1 + solve(nums, curr, curr + 1, dp);
+    //     int include = 0;
+    //     if (prev == -1 || nums[prev] < nums[curr]) {
+    //         include = 1 + solve(nums, curr, curr + 1, dp);
+    //     }
+
+    //     int exclude = solve(nums, prev, curr + 1, dp);
+
+    //     return dp[curr][prev + 1] = max(include, exclude);
+    // }
+
+    int solveusingBS(vector<int>& nums)
+    {
+        vector<int>ans;
+        ans.push_back(nums[0]);
+
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]>ans.back()) ans.push_back(nums[i]);
+
+            else
+            {
+                int index=lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
+                
+            ans[index]=nums[i];
+            }
         }
-
-        int exclude = solve(nums, prev, curr + 1, dp);
-
-        return dp[curr][prev + 1] = max(include, exclude);
+        return ans.size();
     }
 
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        return solve(nums, -1, 0, dp);
+        // int n = nums.size();
+        // vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+        // return solve(nums, -1, 0, dp);
+        return solveusingBS(nums);
     }
 };
