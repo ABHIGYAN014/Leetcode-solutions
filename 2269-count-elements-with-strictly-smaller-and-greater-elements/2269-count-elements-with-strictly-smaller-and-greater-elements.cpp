ee16366533ebeb261c94929333d25c57469a5d2e
Abhigyan Singh <124283757+ABHIGYAN014@users.noreply.github.com>
2025-09-01 00:55:44 +0530

    Time: 0 ms (100%), Space: 12.5 MB (48.53%) - LeetHub

---
+++ 2269-count-elements-with-strictly-smaller-and-greater-elements/2269-count-elements-with-strictly-smaller-and-greater-elements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countElements(vector<int>& nums) {
+        int min=*min_element(nums.begin(),nums.end());
+        int max=*max_element(nums.begin(),nums.end());
+        int count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]<max && nums[i]>min)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
