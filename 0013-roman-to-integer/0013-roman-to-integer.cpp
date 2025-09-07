ed4d8c4e4589b43835ca0feded28fb48df3ee195
Abhigyan Singh <124283757+ABHIGYAN014@users.noreply.github.com>
2025-09-07 16:34:10 +0530

    Time: 7 ms (51.58%), Space: 13.5 MB (18.98%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        // Map for Roman numerals and their corresponding integer values
-        unordered_map<char, int> roman = {
-            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, 
-            {'C', 100}, {'D', 500}, {'M', 1000}
-        };
-        
-        int total = 0;
-        
-        // Traverse the Roman numeral string
-        for (int i = 0; i < s.length(); ++i) {
-            // If the current character's value is less than the next character's value
-            if (i + 1 < s.length() && roman[s[i]] < roman[s[i + 1]]) {
-                total -= roman[s[i]];
-            } else {
-                total += roman[s[i]];
+        unordered_map<char,int>mp=
+        { {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+        int result=0;
+        for(int i=0;i<s.length()-1;i++)
+        {
+            if(mp[s[i]]<mp[s[i+1]])
+            {
+                result-=mp[s[i]];
+            }
+            else
+            {
+                result+=mp[s[i]];
             }
         }
-        
-        return total;
+         result += mp[s.back()];
+        return result;
     }
 };
