0fabe17fe9100e0618055b6e12813663dc4d40b8
Abhigyan Singh <124283757+ABHIGYAN014@users.noreply.github.com>
2025-10-03 16:33:15 +0530

    Time: 0 ms (100%), Space: 14 MB (58.99%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,16 +1,23 @@
-class Solution
-{public:
-vector<int> singleNumber(vector<int> &nums) {
-	unordered_map<int, int> freq;
-	for (auto &num: nums) ++freq[num];
-	vector<int> ans;
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int xorr = 0;
+        for (int num : nums) {
+            xorr ^= num;
+        }
 
-	for (auto &[x, f]: freq)
-		if (f == 1) {
-			ans.push_back(x);
-			if (ans.size() == 2)
-				return ans;
-		}
-	return ans;
-}
+        // compute lowest set bit safely in unsigned arithmetic
+        unsigned int lastbit = (unsigned int)xorr & (-(unsigned int)xorr);
+
+        int b1 = 0, b2 = 0;
+        for (int num : nums) {
+            if ((unsigned int)num & lastbit) {
+                b1 ^= num;
+            } else {
+                b2 ^= num;
+            }
+        }
+
+        return {b1, b2};
+    }
 };
